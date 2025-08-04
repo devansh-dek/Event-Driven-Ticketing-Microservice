eaab5f7dda029c1b56bc13ad553ce05cbe4fbe4c
Devansh Khandelwal <devanshksg@gmail.com>
2025-08-04 10:41:03 +0530

    error middleware and request validation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
+
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middleware/error-handler";
 
 
 const app = express();
@@ -13,6 +16,8 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
+
+app.use(errorHandler)
 
 app.listen(3000, ()=>{
   console.log(`litening on 3000!!!!!!!!!`)
