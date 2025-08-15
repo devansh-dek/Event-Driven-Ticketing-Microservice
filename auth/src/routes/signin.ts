b20c811793591fa39b51535f78c49af9a17ab3e9
Devansh Khandelwal <devanshksg@gmail.com>
2025-08-15 20:39:59 +0530

    siginin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import jwt from 'jsonwebtoken'
+
 import { validateRequest } from '../middleware/validate-request';
+import { User  } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -20,9 +24,39 @@ router.post('/api/users/signin',
 
   validateRequest,
 
-  (req: Request, res: Response)=> {
+  async (req: Request, res: Response)=> {
 
+    const {email, password} = req.body;
+    const existingUser = await User.findOne({email});
 
+    if(!existingUser){
+      throw new BadRequestError('Invalid Credentails')
+    }
+
+    console.log('Exixiting user', existingUser)
+
+    const passwordMatch = await Password.toCompare(
+      existingUser.password, password
+    );
+
+    console.log(2, passwordMatch)
+
+    if(!passwordMatch){
+      throw new BadRequestError("Invalid Credentials")
+    }
+
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    )
+
+    req.session = {
+      jwt: userJwt
+    }
+
+    res.status(201).send(existingUser)
   }
 )
 
