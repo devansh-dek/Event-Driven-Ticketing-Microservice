57356060cfbfd864e74bac2ba3c4011465c0d4cb
Devansh Khandelwal <devanshksg@gmail.com>
2025-08-27 12:18:36 +0530

    [client]  A Reusable API Client

---
+++ client/api/buildClient.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosInstance } from "axios";
+
+interface BuildClientContext {
+  req?: {
+    headers: {
+      [key: string]: string;
+    };
+  };
+}
+
+const buildClient = ({ req }: BuildClientContext): AxiosInstance => {
+  if (typeof window === "undefined") {
+    // Server side
+    return axios.create({
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      headers: req?.headers,
+    });
+  } else {
+    // Client side
+    return axios.create({
+      baseURL: "/",
+    });
+  }
+};
+
+export default buildClient;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,39 @@
+import { cookies } from "next/headers";
+import buildClient from "@/api/buildClient";
 
-export default function Home() {
+export default async function HomePage() {
+  let currentUser = null;
+
+  try {
+    // Use Next.js cookies API
+    const cookieStore =await cookies();
+    const sessionCookie = cookieStore.toString();
+
+    const client = buildClient({
+      req: {
+        headers: {
+          cookie: sessionCookie,
+          host: "ticketing.dev",
+        },
+      },
+    });
+
+    const response = await client.get("/api/users/currentuser");
+    currentUser = response.data.currentUser;
+    console.log('current user ***', currentUser)
+
+  } catch (err) {
+    console.error("Error fetching current user:", err);
+  }
+
   return (
-    <div >
-      Hey buddy!
+    <div>
+      <h1>Home Page</h1>
+      {currentUser ? (
+        <p>Signed in as {currentUser.email}</p>
+      ) : (
+        <p>Not signed in</p>
+      )}
     </div>
   );
 }
