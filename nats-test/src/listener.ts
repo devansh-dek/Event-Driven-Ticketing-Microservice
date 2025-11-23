c58b174e7c9543b23398d4d0f04d6725d60e7340
Devansh Khandelwal <devanshksg@gmail.com>
2025-11-23 20:11:01 +0530

    [nats] manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -12,13 +12,24 @@ const stan = nats.connect(
 stan.on('connect', ()=>{
   console.log('listener! conneted to nats :)');
 
-  const supscription = stan.subscribe('ticket:created', 'litenerQueueGroup');
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+
+
+  const supscription = stan.subscribe(
+    'ticket:created',
+    'litenerQueueGroup',
+    options
+  );
 
   supscription.on('message', (msg: Message)=>{
     const data = msg.getData();
     if(typeof data === 'string'){
       console.log(`Recieved event number`, msg.getSequence(), `with data `, JSON.parse(data))
     }
+
+    msg.ack()
   })
 
 })
